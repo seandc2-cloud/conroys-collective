d6d1a94ef2e7ce8e3fc01259000841f6ed4bf4d5
seandc2-cloud <seandc2@gmail.com>
2026-09-24 15:20:44 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,13 @@
+export const metadata = {
+  title: "Conroy’s Collective",
+  description: "Wearable tech storefront",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
 
