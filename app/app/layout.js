836820fe52d8ca44615a8882c0824887f37b5134
seandc2-cloud <seandc2@gmail.com>
2026-09-24 15:00:37 -0700

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Conroy’s Collective",
+  description: "Wearable tech storefront",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
