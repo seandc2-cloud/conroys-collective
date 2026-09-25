87a449bc446962a8a5304e49795a06c6f1f7c41b
seandc2-cloud <seandc2@gmail.com>
2026-09-25 12:53:30 -0700

    Update page.js

--- app/products/page.js
+++ app/products/page.js
@@ -128,6 +128,19 @@ export default function Products() {
     and helps relieve stress from long workdays and poor posture habits.
   </p>
   <p style={{ fontWeight: "bold" }}>$79.99</p>
+      <button style={{
+  marginTop: "15px",
+  padding: "12px 20px",
+  fontSize: "16px",
+  backgroundColor: "#000",
+  color: "#fff",
+  border: "none",
+  borderRadius: "6px",
+  cursor: "pointer"
+}}>
+  Buy Now
+</button>
+
 </div>
 
       </div>
